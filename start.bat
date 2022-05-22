ab4fe25e969ab4899f1496cc3d5404464c4d2d55
fdasfsdf <99013035+fdasfsdf@users.noreply.github.com>
2022-05-22 23:40:12 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,21 +1,21 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2019 By Network Slutter" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2022 By Michael Jorky" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user administrator @Netslutter /add >nul
-net localgroup administrators administrator /add >nul
-net user administrator /active:yes >nul
+net user freerdp rajahosting2022 /add >nul
+net localgroup administrators freerdp /add >nul
+net user freerdp /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant administrator:F >nul
-ICACLS C:\Windows\installer /grant administrator:F >nul
-echo Successfully Installed !, If the RDP is Dead, Please Rebuild Again!
+ICACLS C:\Windows\Temp /grant freerdp:F >nul
+ICACLS C:\Windows\installer /grant freerdp:F >nul
+echo Success!
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Unable to get the NGROK tunnel, make sure NGROK_AUTH_TOKEN correct in Settings> Secrets> Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels "
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 echo Username: administrator
-echo Password: @Netslutter
-echo Please Login To Your RDP!!
+echo Password: rajahosting2022
+echo You can login now.
 ping -n 10 127.0.0.1 >nul
