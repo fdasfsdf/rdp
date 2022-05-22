faedd0e53dfbb1bd69cfd6e784348a6c29728f04
fdasfsdf <99013035+fdasfsdf@users.noreply.github.com>
2022-05-22 23:46:37 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,21 +1,21 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2022 By Michael Jorky" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By MBAH GADGET" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user freerdp rajahosting2022 /add >nul
-net localgroup administrators freerdp /add >nul
-net user freerdp /active:yes >nul
+net user administrator JohnTech1234 /add >nul
+net localgroup administrators administrator /add >nul
+net user administrator /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant freerdp:F >nul
-ICACLS C:\Windows\installer /grant freerdp:F >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
 echo Success!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 echo Username: administrator
-echo Password: rajahosting2022
+echo Password: JohnTech1234
 echo You can login now.
 ping -n 10 127.0.0.1 >nul
